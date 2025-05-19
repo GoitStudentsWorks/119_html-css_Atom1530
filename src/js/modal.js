86bcb89f28fed742b922829dbabe9b928c5e854a
Samir Sharif <samirsharif6969@gmail.com>
2025-05-18 22:38:53 -0400

    fixed bages 2

--- src/js/modal.js
+++ src/js/modal.js
@@ -18,3 +18,59 @@ openBtns.forEach(btn => {
 closeBtn.addEventListener('click', () => {
   modal.classList.remove('is-open');
 });
+
+
+(() => {
+  // Простая модалка
+  const modalRefs = {
+    openBtn: document.querySelector('[data-modal-open]'),
+    closeBtn: document.querySelector('[data-modal-close-simple]'), // ОБРАТИ ВНИМАНИЕ: другой селектор!
+    modal: document.querySelector('[data-modal]'),
+  };
+
+  modalRefs.openBtn?.addEventListener('click', () => {
+    modalRefs.modal.classList.toggle('is-open');
+    document.body.style.overflow = 'hidden';
+  });
+
+  modalRefs.closeBtn?.addEventListener('click', () => {
+    modalRefs.modal.classList.toggle('is-open');
+    document.body.style.overflow = '';
+  });
+
+  // Бургер-меню
+  const menuRefs = {
+    openBtn: document.querySelector('[data-menu-open]'),
+    closeBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  menuRefs.openBtn?.addEventListener('click', () => {
+    menuRefs.menu.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+  });
+
+  menuRefs.closeBtn?.addEventListener('click', () => {
+    menuRefs.menu.classList.remove('is-open');
+    document.body.style.overflow = '';
+  });
+
+  // Закрытие меню по клику на ссылку
+  const navLinks = document.querySelectorAll('.navbar-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      const targetId = link.getAttribute('href');
+      const targetSection = document.querySelector(targetId);
+
+      menuRefs.menu.classList.remove('is-open');
+      document.body.style.overflow = '';
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+})();
+
